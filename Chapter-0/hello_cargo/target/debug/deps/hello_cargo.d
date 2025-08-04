52c2436f7f81e0a6b54f98721c44dec76143c5fd
Akash <akashgotur512@gmail.com>
2025-08-04 18:39:09 +0530

    chapter 1 completed

---
+++ Chapter-0/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+E:\Learn\Rust\Chapter-0\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+E:\Learn\Rust\Chapter-0\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+src\main.rs:
---
+++ Chapter-0/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+E:\Learn\Rust\Chapter-0\hello_cargo\target\debug\hello_cargo.exe: E:\Learn\Rust\Chapter-0\hello_cargo\src\main.rs
