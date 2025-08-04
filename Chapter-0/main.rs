ff26e3fc7a567eb5321c59351724bedd5afeec11
Akash <akashgotur512@gmail.com>
2025-08-04 18:23:20 +0530

    Learn Rust

---
+++ Chapter-0/main.rs
@@ -0,0 +1,21 @@
+ // ?Hello World program
+
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+    ? The main function (fn main() {}) is the starting point of every executable Rust program.
+
+    ? Function bodies in Rust are enclosed in curly brackets ({}), and it’s good style to place the opening bracket on the same line as the function declaration.
+
+    ? Code formatting is standardized with a tool called rustfmt, which is included with Rust.
+
+    ? The line println!("Hello, world!"); prints text to the screen by calling a macro (note the exclamation mark).
+
+    ? Macros (like println!) use ! and are different from ordinary functions.
+
+    ? Most lines in Rust end with a semicolon (;).
+
+*/ 
+
